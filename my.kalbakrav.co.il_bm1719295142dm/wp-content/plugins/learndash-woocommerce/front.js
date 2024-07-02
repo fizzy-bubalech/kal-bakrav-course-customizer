cef8e28de5f01dfe0517bbb2380e6201eed94098
fizzy-bubalech <av.trumper@gmail.com>
2024-07-02 20:10:46 +0300

    bruh

--- my.kalbakrav.co.il_bm1719295142dm/wp-content/plugins/learndash-woocommerce/front.js
+++
@@ -1,9 +0,0 @@
-jQuery(function ($) {
-	const accountSwitch = $('input#createaccount');
-	if (accountSwitch.length) {
-		accountSwitch.attr('checked', true).parent().hide();
-
-		$('.create-account').show();
-		accountSwitch.parent().hide();
-	}
-});
